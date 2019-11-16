34370bf81d84e8530d63ad8cefef46c29c3a35d3
Nick Holke <nickholke@gmail.com>
2019-11-16 15:39:49 -0800

    Set up server and nodemon

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+const path = require('path');
+
+app.get('/', (req,res) => res.send('Hello there'));
+
+app.use('/', express.static(path.resolve(__dirname, '../public')));
+
+app.listen(port, () => console.log(`listening on port ${port}`));
